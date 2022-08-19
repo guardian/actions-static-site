42ab7dcc96cfe9e875cd4aac29fbea4d17b2bd10
Nicolas Long <nicolas.long@theguardian.com>
2022-08-19 13:21:35 +0100

    Update layer path

--- lambda/main.go
+++ lambda/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("/opt")) // lambda layers are installed here
+	fs := http.FileServer(http.Dir("/opt/site")) // (the site layer)
 	http.Handle("/", fs)
 
 	// http.ListenAndServe("localhost:3030", nil)
