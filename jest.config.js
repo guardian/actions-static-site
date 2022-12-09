21bdddd0040cd5b9807eba46af1044df33b9ee04
frederickobrien <frederick.obrien@guardian.co.uk>
2022-12-09 16:24:21 +0000

    add bucket policy to allow Galaxies to write to the `data` directory of its area of static bucket
    Co-Authored-By: Tom Richards <19289579+twrichards@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFilesAfterEnv: ["./jest.setup.js"],
+  transformIgnorePatterns: ["node_modules/(?!@guardian/private-infrastructure-config)"]
 };
