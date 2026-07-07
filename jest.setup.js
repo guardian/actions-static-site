8dde06c1176d51c4afaa9163dc17a29046a0c4fa
akash1810 <akash1810@users.noreply.github.com>
2026-07-07 08:58:28 +0100

    test: Setup GuCDK version mock

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,4 @@
 /* eslint-disable no-undef -- jest globals are missing due to guardian.config.jest eslint rules not covering jest.setup.js - see https://github.com/guardian/csnx/blob/main/libs/%40guardian/eslint-config/configs/jest.js */
 
 jest.mock('@guardian/aws-account-setup');
+jest.mock('@guardian/cdk/lib/constants/tracking-tag');
